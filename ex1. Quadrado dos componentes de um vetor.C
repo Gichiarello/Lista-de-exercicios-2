6a0e79dc0f8c8e06ef3880b88b5080c774166607
Giovanni Chiarello <113187714+Gichiarello@users.noreply.github.com>
2023-05-31 19:47:08 -0300

    Update ex1. Quadrado dos componentes de um vetor.C

--- ex1. Quadrado dos componentes de um vetor.C
+++ ex1. Quadrado dos componentes de um vetor.C
@@ -1,3 +1,6 @@
+/*Giovanni Chiarello 31/05
+Finalidade: calcula o quadrado de todos os componentes dentro de um vetor
+*/
 #include <stdio.h>
 #include <locale.h>
 int main(){
@@ -6,7 +9,7 @@ int main(){
 	float quadrado[10];
 	printf("Digite 10 valores Reais:\n");
 	for(i=0;i<10;i++){
-		printf("Insira o %d° numero: ",i+1); scanf("%d", &array[i]);
+		printf("Insira o %dÂ° numero: ",i+1); scanf("%d", &array[i]);
 	}
 	for(i=0;i<10;i++){
 		array[i]= array[i]*array[i];
